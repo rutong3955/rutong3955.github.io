bf2eef56fe888f0d6312e76f20e32de2576fc44c
rutong3955 <rutongceee@gmail.com>
2026-02-10 19:53:04 +0000

    Deploying to gh-pages from @ rutong3955/rutong3955.github.io@e178715f25db16335793cb0155cf3f9c590a785e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-",
-          title: "",
+        },{id: "nav-teaching",
+          title: "teaching",
           description: "",
           section: "Navigation",
           handler: () => {
